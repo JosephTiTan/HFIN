b68e7f7584bd084dc4f6b5cb8b5113b5f7b470fb
JosephTiTan <163397520+JosephTiTan@users.noreply.github.com>
2024-06-18 19:10:52 +0800

    Update train.py

--- training/train.py
+++ training/train.py
@@ -23,7 +23,7 @@ import sys
 
 sys.path.append("E:/Project/Pansharpening/clswin")
 
-from models.clswin_lap50 import Net
+from models.HFIN import Net
 
 from metrics import get_metrics_reduced
 from metrics import fin_metrics_reduced
@@ -37,15 +37,9 @@ Configure our network
 ------------------------------------------------------------------------------
 '''
 
-model_str = 'clswin_lap50'
-satellite_str = 'WV2'   # WV2 WV3 GF2  0 1 2
+model_str = 'HFIN'
+satellite_str = 'WV2'   # WV2 WV3 GF2
 
-# . Get the parameters of your satellite
-# sat_param = get_sat_param(satellite_str)
-# if sat_param!=None:
-#     ms_channels, pan_channels, scale = sat_param
-# else:
-#     print('You should specify `ms_channels`, `pan_channels` and `scale`! ')
 
 alfa = 0.1
 ms_channels = 4
@@ -54,7 +48,7 @@ scale = 4
 
 # . Set the hyper-parameters for training
 num_epochs = 1000
-lr = 5e-4
+lr = 1e-3
 print('lr is', lr)
 weight_decay = 0
 batch_size = 4
@@ -67,49 +61,23 @@ torch.cuda.set_device(0)
 net = Net(4).to(device)  #channels=4 for gppnn
 print(net)
 
-#predir = '/ghome/tanjt/code/GPPNN/training/pretrain4'
-#if os.path.exists(predir):
-#    net.load_state_dict(torch.load(os.path.join(predir, 'best_net.pth'))['net'],strict=False)
-#    print("load pretrained model successfully")
-
 # . Get your optimizer, scheduler and loss function
 optimizer = torch.optim.Adam(net.parameters(), lr=lr, weight_decay=weight_decay)
 scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=200, gamma=0.5)
 loss_fn = nn.L1Loss().to(device)
 
 # . Create your data loaders
-# prepare_data_flag = False # set it to False, if you have prepared dataset
-# train_path = '../PS_data/%s/%s_train.h5'%(satellite_str,satellite_str)
-# train_path = '/home/jieh/Projects/PAN_Sharp/GPPNN-main/PS_data/data/%s/train.mat'%(satellite_str)
-# #validation_path = '../PS_data/%s/validation'%(satellite_str)
-# # validation_path = '/home/jieh/Projects/PAN_Sharp/GPPNN-main/PS_data/data/%s/test.mat'%(satellite_str)
-# test_path = '/home/jieh/Projects/PAN_Sharp/GPPNN-main/PS_data/data/%s/test.mat'%(satellite_str)
-
-
-# if prepare_data_flag is True:
-#     prepare_data(data_path = '../PS_data/%s'%(satellite_str),
-#                  patch_size=32, aug_times=1, stride=32, synthetic=False, scale=scale,
-#                  file_name = train_path)
 data_dir_ms_train = 'E:/Project/Pansharpening/yaogan/WV2_data/train128/ms/'
 data_dir_pan_train = 'E:/Project/Pansharpening/yaogan/WV2_data/train128/pan/'
-# trainloader = DataLoader(PSH5Datasetfu(train_path),
-#                               batch_size=batch_size,
-#                               shuffle=True) #[N,C,K,H,W]
+
 trainloader = DataLoader(Data(data_dir_ms=data_dir_ms_train, data_dir_pan=data_dir_pan_train),
                          batch_size=batch_size,
                          shuffle=True)
 
-# validationloader = DataLoader(PSH5Datasetfu(validation_path),
-#                               batch_size=1) #[N,C,K,H,W]
 data_dir_ms_test = 'E:/Project/Pansharpening/yaogan/WV2_data/test128/ms/'
 data_dir_pan_test = 'E:/Project/Pansharpening/yaogan/WV2_data/test128/pan/'
 testloader = DataLoader(Data(data_dir_ms=data_dir_ms_test, data_dir_pan=data_dir_pan_test),
                         batch_size=1)  # [N,C,K,H,W]
-
-# validationloader = DataLoader(PSDataset(validation_path,scale),
-#                               batch_size=1)
-# testloader = DataLoader(PSDataset(test_path, scale),
-#                         batch_size=1)
 
 loader = {'train': trainloader,
           'validation': testloader}
@@ -147,11 +115,6 @@ for epoch in range(num_epochs):
     for i, (ms, pan, gt) in enumerate(loader['train']):
         # 0. preprocess data
         ms, pan, gt = ms.cuda(), pan.cuda(), gt.cuda()
-        # print('shape is ', ms.shape, pan.shape, gt.shape)
-        # ms,_ = normlization(ms.cuda())
-        # pan,_ = normlization(pan.cuda())
-        # gt,_ = normlization(gt.cuda())
-
         # 1. update
         net.train()
         net.zero_grad()
@@ -159,9 +122,6 @@ for epoch in range(num_epochs):
         #[4, 4, 32, 32]) torch.Size([4, 1, 128, 128])
         predHR= net(ms, pan)
         loss_forward = loss_fn(predHR, gt.detach())
-        # backHR, mHR, Lpan = net(ms, pan, gt.detach(), rev=True)
-        # loss_backward = (loss_fn(backHR[:,:4,:,:], mHR.detach())+loss_fn(backHR[:,4:,:,:], Lpan.detach()))*0.1
-
         loss = loss_forward
         loss.backward()
         optimizer.step()
@@ -203,9 +163,7 @@ for epoch in range(num_epochs):
         net.eval()
         for i, (ms, pan, gt) in enumerate(testloader):
             ms, pan, gt = ms.cuda(), pan.cuda(), gt.cuda()
-            # ms,_ = normlization(ms.cuda())
-            # pan,_ = normlization(pan.cuda())
-            # gt,_ = normlization(gt.cuda())
+
             predHR = net(ms, pan)
             metrics[:, i] = torch.Tensor(get_metrics_reduced(predHR, gt))[:2]
         psnr_val, ssim_val = metrics.mean(dim=1)
@@ -289,19 +247,3 @@ print("list_qindex_mean:", np.mean(list_Q))
 print("list_dlama_mean:", np.mean(list_DL))
 print("list_ds_mean:", np.mean(list_DS))
 print("list_qnr_mean:", np.mean(list_QNR))
-# 3. Write the metrics
-# f = xlwt.Workbook()
-# sheet1 = f.add_sheet(u'sheet1',cell_overwrite_ok=True)
-# img_name = [i.split('\\')[-1].replace('.mat','') for i in testloader.dataset.files]
-# metric_name = ['PSNR','SSIM','CC','SAM','ERGAS']
-# for i in range(len(metric_name)):
-#     sheet1.write(i+1,0,metric_name[i])
-# for j in range(len(img_name)):
-#    sheet1.write(0,j+1,img_name[j])
-# for i in range(len(metric_name)):
-#     for j in range(len(img_name)):
-#         sheet1.write(i+1,j+1,float(metrics[i,j]))
-# sheet1.write(0,len(img_name)+1,'Mean')
-# for i in range(len(metric_name)):
-#     sheet1.write(i+1,len(img_name)+1,float(metrics.mean(1)[i]))
-# f.save(os.path.join(save_path,'test_result.xls'))
